2fad2373202e7576f2c4b1e6c7bb1a826f030038
Max_Work <liuping@mizhitech.com>
2023-12-25 14:29:00 +0800

    1.b

--- part1/src/App.js
+++ part1/src/App.js
@@ -7,11 +7,13 @@ const Header = (prop) => {
 }
 
 const Content = (prop) => {
+  console.log(prop)
+  const part = prop.parts;
   return (
     <div>
-      <Part name="part0" times={0}></Part>
-      <Part name="part1" times={1}></Part>
-      <Part name="part2" times={2}></Part>
+      <Part name={part[0].name} times={part[0].exercises}></Part>
+      <Part name={part[1].name} times={part[1].exercises}></Part>
+      <Part name={part[2].name} times={part[2].exercises}></Part>
     </div>
   )
 }
@@ -33,11 +35,26 @@ const Total = (prop) => {
 
 
 const App = () => {
-  const course_name = "Full Stack Open 2023";
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
   return (
     <div>
-      <Header name={course_name}></Header>
-      <Content></Content>
+      <Header name={course}></Header>
+      <Content parts={parts}></Content>
       <Total></Total>
     </div>
   )
